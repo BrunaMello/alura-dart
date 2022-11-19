bb8e23d6c58bbb65d80da310dcdb5a0dcf759459
brunamello <brunacrespomello@gmail.com>
2022-11-19 20:15:45 +0000

    3 - Lists

--- bin/alura_dart.dart
+++ bin/alura_dart.dart
@@ -2,7 +2,6 @@ void main() {
   //numbers on dart: int and double
   //dart accepts hexadecimal, exponential
   //need have a value
-
   int age = 31;
   print(age);
 
@@ -12,14 +11,29 @@ void main() {
   print(value);
 
   //Boolean value
-
   bool geek = true;
   bool comparison = (age == height);
   print(comparison);
 
   //String value
-
-  String name = 'Bruna';
+  const String name = 'Bruna Mello';
   print('I am: $name. \nI am $age years old.' );
+  
+  // Lists 
+  List<String> nameslist = ['Bruna', 'Marcelo', 'Carlos', 'Ana']; //only accept strings
+  print(nameslist[3]);
+  print(nameslist.length);
+  print(nameslist);
+
+  // Dynamics Lists
+  // Not a good practice. Avoid
+  List<dynamic> bruna = [age, height, geek, name];
+  print('I am ${bruna[3]}');
+
+  // Typos var, const and final
+  // var we can change the type of the variable. Not a good practice.
+  // const we cannot change
+  // final we cannot change, can be null when started
+
 
 }

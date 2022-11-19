30579df271c0ffc251a0950462fbac4373867496
brunamello <brunacrespomello@gmail.com>
2022-11-19 19:55:11 +0000

    2 - Variables

--- bin/alura_dart.dart
+++ bin/alura_dart.dart
@@ -1,3 +1,25 @@
 void main() {
-  print('Hello world!');
+  //numbers on dart: int and double
+  //dart accepts hexadecimal, exponential
+  //need have a value
+
+  int age = 31;
+  print(age);
+
+  double height = 170.0;
+  double value = 780e6;
+  print(height);
+  print(value);
+
+  //Boolean value
+
+  bool geek = true;
+  bool comparison = (age == height);
+  print(comparison);
+
+  //String value
+
+  String name = 'Bruna';
+  print('I am: $name. \nI am $age years old.' );
+
 }

752a850efee81824f8759a74c7b2ab2361ecc46d
brunamello <brunacrespomello@gmail.com>
2022-11-19 19:32:44 +0000

    1 - Functions and Environments.

--- bin/alura_dart.dart
+++ bin/alura_dart.dart
@@ -1,5 +1,3 @@
-import 'package:alura_dart/alura_dart.dart' as alura_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${alura_dart.calculate()}!');
+void main() {
+  print('Hello world!');
 }

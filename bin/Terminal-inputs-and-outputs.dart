6320ca3dbd173b779c0c53b79b1a35cc8abd0224
brunamello <brunacrespomello@gmail.com>
2022-11-22 01:29:04 +0000

    7 - Terminal inputs and outputs 2

--- bin/Terminal-inputs-and-outputs.dart
+++ bin/Terminal-inputs-and-outputs.dart
@@ -3,5 +3,15 @@ import 'dart:io';
 void main() {
   print("Whats my age?");
   String? input = stdin.readLineSync();
-  print("My age is: $input years old.");
+  if(input != null) {
+    print("My age is: $input years old.");
+    int idade = int.parse(input);
+    print("Your age next year will be: ${idade + 1} years old.");
+  }
+
+
+
+
+
 }
+
